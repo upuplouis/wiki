649de60f5b9f621fcccc4e22c83e8f6b2a36c084
yliu <617920405@qq.com>
2019-09-27 13:53:45 +0800

    fix chart

--- grpc-gateway/src/main.go
+++ grpc-gateway/src/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/rs/cors"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"handlers"
 	"net"
 	"net/http"
 	"protos"
 	"strconv"
+	"strings"
 	"sync"
 	"utils"
 )
@@ -30,7 +34,8 @@ func startHttpServer(httpPort int, grpcEndpoint string) error {
 	ctx := context.Background()
 	ctx, cancle := context.WithCancel(ctx)
 	defer cancle()
-	mux := runtime.NewServeMux()
+	mux := runtime.NewServeMux(runtime.WithProtoErrorHandler(ProtoErrorHandler),
+		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{OrigName: true, EmitDefaults: true}))
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 	err := protos.RegisterGetwayServiceHandlerFromEndpoint(ctx, mux, grpcEndpoint, opts)
 	if err != nil {
@@ -51,7 +56,51 @@ func startHttpServer(httpPort int, grpcEndpoint string) error {
 		AllowCredentials: true,
 	})
 	handler := c.Handler(mux)
+	handler = GetTokenServer(handler)
+
+	fileServer := http.StripPrefix("", http.FileServer(http.Dir("")))
+	handler = http.StripPrefix("", handler)
+	handler = setFileServer(fileServer, handler)
+
 	return http.ListenAndServe(fmt.Sprintf(":%d", httpPort), handler)
+}
+
+func setFileServer(fileServer, other http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.RequestURI, "") {
+			fileServer.ServeHTTP(w, r)
+		}else {
+			other.ServeHTTP(w, r)
+		}
+	})
+}
+
+func GetTokenServer(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodOptions {
+			r.Header.Set("Pragma", strings.TrimSpace(r.Header.Get("token")))
+			//md := metadata.Pairs("token", token)
+			//ctx := metadata.NewOutgoingContext(r.Context(), md)
+			//r = r.WithContext(ctx)
+			handler.ServeHTTP(w, r)
+		}else {
+			handler.ServeHTTP(w, r)
+		}
+	})
+}
+
+func ProtoErrorHandler(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
+	s, ok := status.FromError(err)
+	if !ok || s.Code() != codes.OK {
+		message := new(interface{})
+		w.Header().Del("Trailer")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		bs, _ := json.Marshal(message)
+		_, err = w.Write(bs)
+	}else {
+		runtime.DefaultHTTPProtoErrorHandler(ctx, mux, marshaler, w, r, err)
+	}
 }
 
 func main() {
